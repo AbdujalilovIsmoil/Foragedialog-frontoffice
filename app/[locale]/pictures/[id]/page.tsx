452d39a6cafca4c363fdaed2c5bb8ebf1bfe1345
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2025-10-06 18:23:45 +0500

    feat

--- app/[locale]/pictures/[id]/page.tsx
+++ app/[locale]/pictures/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useParams, usePathname } from "next/navigation";
 import { Button } from "@/app/components";
